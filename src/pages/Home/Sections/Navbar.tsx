b2781c9f2e7aed4e570618e5d438af42dcbec6ac
Dagson Gabriel <dagson.g.01@gmail.com>
2024-04-18 22:07:53 -0300

    Create Navbar

---
+++ src/pages/Home/Sections/Navbar.tsx
@@ -0,0 +1,99 @@
+import * as React from 'react';
+import { AppBar, Avatar, Box, Button, Container, Toolbar, Typography } from '@mui/material';
+import AdbIcon from '@mui/icons-material/Adb';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import TemporaryDrawer from './Drawer';
+import Perfil from './../../../assets/eu.png';
+
+const pages = ['home', 'projects', 'skills', 'services', 'about' ,'contact'];
+
+interface ElevationScrollProps {
+  children: React.ReactElement;
+  window?: () => Window;
+}
+
+function ElevationScroll(props: ElevationScrollProps) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 10 : 0,
+    style: { visibility: trigger ? 'visible' : 'hidden' },
+  });
+}
+
+function ResponsiveAppBar() {
+
+  return (
+    <ElevationScroll>
+      <AppBar position='fixed'>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              DAGSON
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>  
+              <TemporaryDrawer />
+            </Box>
+            <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.2rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              DAGSON
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  href={`#${page}`}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Avatar alt="Remy Sharp" src={ Perfil } />
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ElevationScroll>
+  );
+}
+
+export default ResponsiveAppBar;
