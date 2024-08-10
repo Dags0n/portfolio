211f2edd676ab6b4ef31119418772e8345a48b47
Dagson Gabriel <dagson.g.01@gmail.com>
2024-08-10 05:02:05 -0300

    Refactor Header component layout and styles

--- src/pages/Home/Sections/Header.tsx
+++ src/pages/Home/Sections/Header.tsx
@@ -24,33 +24,37 @@ const Header = () => {
       <StyledDiv id='home'>
         <Container maxWidth="lg">
           <Grid container>
-            <Grid item xs={12} md={8}>
-              <Typography color="primary.contrastText" variant="h4" sx={{ textAlign: { xs: 'center', md: 'left' }, marginLeft: { xs: '0px', md: '50px' } }}>Hello World!</Typography>
-              <Typography color="primary.contrastText" variant="h1" sx={{ textAlign: { xs: 'center', md: 'left' }, marginLeft: { xs: '0px', md: '50px' } }}>I'm Dagson</Typography>
-              <Typography color="primary.contrastText" variant="h3" sx={{ textAlign: { xs: 'center', md: 'left' }, marginLeft: { xs: '0px', md: '50px' } }} pt="10px">Software Developer</Typography>
-              <Grid container >
-                <Grid item xs={12} md={5} display="flex" justifyContent="center" pt="30px">
-                  <Button variant='contained' color='secondary' href={require('./../../../assets/curriculo-dagson.pdf')} download="curriculo-dagson.pdf">
-                    <DownloadIcon />
-                    <Typography color="primary.contrastText" pl='10px'>Download CV</Typography>
-                  </Button>
-                </Grid>
-                <Grid item xs={12} md={5} display={{ xs: 'none', md: 'block' }} pt="30px" >
-                  <Button variant='contained' color='secondary' href='https://www.linkedin.com/in/dagson1/' target='_blank'>
-                    <LinkedinIcon />
-                    <Typography color="primary.contrastText" pl='10px'>Linkedin</Typography>
-                  </Button>
+            <Grid item xs={12} sm={5}>
+              <Box position="relative">
+                <Box position="relative" textAlign="center">
+                  <StyledImg src={Perfil} />
+                </Box>
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={7} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+              <Typography color="primary.contrastText" variant="h4" sx={{ textAlign: { xs: 'center', md: 'center' }, marginLeft: { xs: '0px', md: '50px' } }}>Hello World!</Typography>
+              <Typography color="primary.contrastText" variant="h1" sx={{ textAlign: { xs: 'center', md: 'center' }, marginLeft: { xs: '0px', md: '50px' } }}>I'm Dagson</Typography>
+              <Typography color="primary.contrastText" variant="h3" sx={{ textAlign: { xs: 'center', md: 'center' }, marginLeft: { xs: '0px', md: '50px' } }} pt="10px">Software Developer</Typography>
+            </Grid>  
+            <Grid container sx={{ display: 'flex', justifyContent: { xs: 'center', md: 'flex-start' } }}>
+              <Grid item xs={12} sm={5}>
+                <Grid container spacing={2} justifyContent="center" pt='30px'>
+                  <Grid item sx={{ pb: { xs: '30px', md: '0' } }}>
+                    <Button variant='contained' color='secondary' href={require('./../../../assets/curriculo-dagson.pdf')} download="curriculo-dagson.pdf">
+                      <DownloadIcon />
+                      <Typography color="primary.contrastText" pl='10px'>Download CV</Typography>
+                    </Button>
+                  </Grid>
+                  <Grid item display={{ xs: 'none', md: 'block' }}>
+                    <Button variant='contained' color='secondary' href='https://www.linkedin.com/in/dagson1/' target='_blank'>
+                      <LinkedinIcon />
+                      <Typography color="primary.contrastText" pl='10px'>Linkedin</Typography>
+                    </Button>
+                  </Grid>
                 </Grid>
               </Grid>
-            </Grid>  
-            <Grid item xs={12} md={4}>
-                <Box position="relative">
-                  <Box position="relative" textAlign="center">
-                    <StyledImg src={Perfil} />
-                  </Box>
-                </Box>
             </Grid>
-          </Grid>          
+          </Grid>
         </Container>
       </StyledDiv>
     </>
