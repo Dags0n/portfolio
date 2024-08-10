5de04a08e9dc14ff41a4742fea750e36f201efbd
Dagson Gabriel <dagson.g.01@gmail.com>
2024-08-10 05:14:48 -0300

    Update experience timeline order

--- src/pages/Experience/Sections/Timeline.tsx
+++ src/pages/Experience/Sections/Timeline.tsx
@@ -30,8 +30,11 @@ export default function NoOppositeContent() {
           <Typography variant="h5" component="span" fontWeight="bold">
             Full-Stack Developer
           </Typography>
-          <Typography mb="20px">
+          <Typography>
             DCODING
+          </Typography>
+          <Typography mb="20px">
+            March 2024 - Present
           </Typography>
           <Typography maxWidth="500px" textAlign="justify">
             I actively work in a company in formation, being responsible for the architecture and development of web applications. My main responsibilities include:
@@ -60,39 +63,13 @@ export default function NoOppositeContent() {
         </TimelineSeparator>
         <TimelineContent>
           <Typography variant="h5" component="span" fontWeight="bold">
-            Project Researcher
-          </Typography>
-          <Typography mb="20px">
-            Federal Institute of Rio Grande do Norte (IFRN)
-          </Typography>
-          <Typography maxWidth="500px" textAlign="justify">
-            I developed a web application aimed at managing inventory and sales for micro-enterprises, with the aim of optimizing and simplifying resource management and commercial operations processes. This project involved:
-          </Typography>
-          <Typography>
-          <List>
-            <ListItem sx={{ mb: '-15px', maxWidth: '500px', textAlign: 'justify' }}>
-              <ListItemText primary="• Creation of the web application using HTML, CSS, JavaScript, PHP and MySQL." />
-            </ListItem>
-            <ListItem sx={{ maxWidth: '500px', textAlign: 'justify' }}>
-              <ListItemText primary="• Presentation of the project at a national congress, highlighting the importance of the solution." />
-            </ListItem>
-          </List>     
-          </Typography>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot color="primary">
-            <AutoStoriesOutlinedIcon />
-          </TimelineDot>
-          <TimelineConnector sx={{ bgcolor: '#1e88e5' }}/>
-        </TimelineSeparator>
-        <TimelineContent>
-          <Typography variant="h5" component="span" fontWeight="bold">
             Physics Tutor
           </Typography>
-          <Typography mb="20px">
+          <Typography>
             Federal Institute of Rio Grande do Norte (IFRN)
+          </Typography>
+          <Typography mb="20px">
+            June 2022 - February 2023
           </Typography>
           <Typography maxWidth="500px" textAlign="justify">
             I helped students achieve a deeper understanding of physics concepts. My responsibilities included:
@@ -107,6 +84,38 @@ export default function NoOppositeContent() {
             </ListItem>
             <ListItem>
               <ListItemText primary="• Facilitate review sessions for test and exam preparations." />
+            </ListItem>
+          </List>     
+          </Typography>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot color="primary">
+            <AutoStoriesOutlinedIcon />
+          </TimelineDot>
+          <TimelineConnector sx={{ bgcolor: '#1e88e5' }}/>
+        </TimelineSeparator>
+        <TimelineContent>
+          <Typography variant="h5" component="span" fontWeight="bold">
+            Project Researcher
+          </Typography>
+          <Typography>
+            Federal Institute of Rio Grande do Norte (IFRN)
+          </Typography>
+          <Typography mb="20px">
+            May 2022 - October 2022
+          </Typography>
+          <Typography maxWidth="500px" textAlign="justify">
+            I developed a web application aimed at managing inventory and sales for micro-enterprises, with the aim of optimizing and simplifying resource management and commercial operations processes. This project involved:
+          </Typography>
+          <Typography>
+          <List>
+            <ListItem sx={{ mb: '-15px', maxWidth: '500px', textAlign: 'justify' }}>
+              <ListItemText primary="• Creation of the web application using HTML, CSS, JavaScript, PHP and MySQL." />
+            </ListItem>
+            <ListItem sx={{ maxWidth: '500px', textAlign: 'justify' }}>
+              <ListItemText primary="• Presentation of the project at a national congress, highlighting the importance of the solution." />
             </ListItem>
           </List>     
           </Typography>
